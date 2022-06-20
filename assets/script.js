c855be5bfe67fe22ff4636ce1cc8a4eb30c03e96
ray_han <ray102303@gmail.com>
2022-06-20 21:22:16 +0700

    commitKetiga

--- assets/script.js
+++ assets/script.js
@@ -12,55 +12,41 @@ const nav = document.querySelector('nav ul');
  const dropdownContent = document.querySelector('li .dropdown-content')
  dropdown.addEventListener('click', function(e){
    dropdownContent.classList.toggle('screen');
-   e.preventDefault();
  });
+
+
+ let show = false;
 
  const menuName = document.querySelectorAll('main #content p a');
  menuName.forEach(function(m,i){
-   m.addEventListener('click', function(){
+  if(!show){
+    m.addEventListener('click', function(){
       const card = document.createElement('div');
       document.body.appendChild(card);
       card.setAttribute('class', 'card-new');
-      card.classList.add('display');
+      card.classList.toggle('display');
+
       const img = document.createElement("img");
       img.src = `assets/img/menu/${i}.jpg`;
-      img.classList.toggle("img-width");
+      img.classList.add("img-width");
       card.appendChild(img);
       document.body.appendChild(card);
-      
-      
+      const textEl = document.createElement('h3');
+      textEl.innerText = m.innerText;
+      card.appendChild(textEl);
+      show = 'true';
     });
+  }
  });
- 
 
+    let btnBody = document.querySelectorAll('body .card-new');
+    if(show){
+      document.body.addEventListener('click', function(){
+      removeChild(btnBody);
+    })
+    }
+     
 
-
-
-
-
-// let srcImg = () => { 
-//    for(let i = 1; i < isiMenu.length+1; i++){
-//       const img = document.createElement("img");
-//       img.src = `assets/img/menu/${i}.jpg`;
-//       document.body.appendChild(img);
-//       img.classList.add("img-width");
-//    };
-// }
-
-// Smoothscroll
-// const navLink = document.querySelectorAll('.nav-link');
-// navLink.forEach(function(i){
-//    i.addEventListener('click', function(){
-//       let tujuan = this.getAttribute("href");
-//       let elemenTujuan = tujuan;
-//       console.log(i);
-//       document.body.stop().animate({scrollTop: elemenTujuan.offset().top - 50},1000, 'easeInOutSine', function() { 
-//       });
-      
-//       e.preventDefault();
-      
-//    });
-// });
 });
 
 
